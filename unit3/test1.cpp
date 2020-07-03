8c3256257a5c3d0c1c21cbb543ee74b2e06f3e77
ckunlun <ckunlun@outlook.com>
2020-07-03 08:21:27 +0800

    第三单元，只有课后作业题

---
+++ unit3/test1.cpp
@@ -0,0 +1,44 @@
+﻿//学习opencv书本课后作业
+//ckunlun
+//2020.6.30
+//练习1：
+//c++的数据处理
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace std;
+
+int main()
+{
+    float test = - 1.5; 
+    float testAbs = abs(test);
+    //int testInt1 = fix(test); //朝零方向取整，报错提示无此函数
+    int testInt2 = floor(test); //向下取整
+    int testInt3 = ceil(test); //向上取整
+    int testInt4 = round(test); //朝零方向取整
+
+    srand((int)time(0)); //产生随机种子，把0换成NULL也行。
+    //这样保证每次生成的随机数不一样。
+    int myRand = rand();  //产生随机数
+
+    //生成浮点数的cv::Point2f,把它转化成整形的cv::Point
+    //把cv::Pointz转换成cv::Point2f
+    cv::Point2f testPoint(1.1, 2.2); //值函数构造
+    cv::Point pointInt(testPoint); //复制构造函数
+    
+    cout << test << endl << testAbs << endl;
+    cout << testInt2 << endl << testInt3 << endl;
+    cout << testInt4 << endl << myRand << endl;
+    cout << testPoint << endl << pointInt << endl;
+
+ /*   -1.5
+      1.5
+      - 2
+      - 1
+      - 2
+      13831
+      [1.1, 2.2]
+      [1, 2]*/
+    return 0;
+    
+}
+
---
+++ unit3/test3.cpp
@@ -0,0 +1,33 @@
+//学习opencv书本课后作业
+//ckunlun
+//2020.6.30
+//练习3：
+//紧凑型矩阵和向量模板类
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace std;
+
+int main()
+{
+    cv::Matx33f myMat(1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9); //生成一个3*3的矩阵
+    cv::Matx31f myMec(1.1, 2.2, 3.3); //生成一个3维向量
+    cv::Vec3f myVec(1.1, 2.2, 3.3); //生成一个3维向量
+
+    cv::Matx31f zero31 = cv::Matx31f::zeros(); //创建一个全零矩阵
+    zero31 = myMat * myVec; //矩阵乘法
+    zero31 = myMat * myMec; //矩阵乘法
+
+    cout << myMat << endl << zero31 << endl;
+
+    /*   运行结果：
+       [1.1, 2.2, 3.3;
+       4.4000001, 5.5, 6.5999999;
+       7.6999998, 8.8000002, 9.8999996]
+       [16.939999;
+       38.720001;
+       60.5]*/
+    return 0;
+
+}
+
